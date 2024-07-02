385e4fb985d2b64dd539613ce50f79c06bcfba86
manonaSamir <bena.yalla@yahoo.com>
2024-07-02 14:42:58 +0300

    fix

--- 0x00-ES6_basic/3-main.js
+++ 0x00-ES6_basic/3-main.js
@@ -1,6 +1,5 @@
 import getSumOfHoods from './3-default-parameter.js';
 
 console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(10));
 console.log(getSumOfHoods(34, 3));
 console.log(getSumOfHoods(34, 3, 4));
