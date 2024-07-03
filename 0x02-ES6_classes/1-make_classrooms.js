6e8649ad25ab4dd4966ed12faa7006dbe391d2e1
manonaSamir <bena.yalla@yahoo.com>
2024-07-03 22:03:58 +0300

    make classs room

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,8 @@
+import ClassRoom from "./0-classroom";
+
+/**
+ * Creates an array with a specific size.
+ */
+export default function initializeRooms() {
+  return [19, 20, 34].map((size) => new ClassRoom(size));
+}
