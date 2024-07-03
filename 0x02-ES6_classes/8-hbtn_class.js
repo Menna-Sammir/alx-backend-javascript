57d650d4138bb80b9638bbf31c37a0b6dcbee973
manonaSamir <bena.yalla@yahoo.com>
2024-07-03 22:34:45 +0300

    holberton class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,30 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(size) {
+    this._size = size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(location) {
+    this._location = location;
+  }
+
+  toString() {
+    return this.location;
+  }
+
+  valueOf() {
+    return this.size;
+  }
+}
