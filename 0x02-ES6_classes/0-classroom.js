80bdc9273db706aaa3319a6c51d6b314cf4d90d6
manonaSamir <bena.yalla@yahoo.com>
2024-07-03 20:24:20 +0300

    create first class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,11 @@
+/**
+ * class room.
+ */
+export default class ClassRoom {
+  /** The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
