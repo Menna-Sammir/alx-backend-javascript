a8acd34f9392d55415b5f93d22b16667f96f082e
manonaSamir <bena.yalla@yahoo.com>
2024-07-04 16:34:49 +0300

    get list by student id

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+/**
+ * Retrieves ids from a list of students.
+ */
+export default function getListStudentIds(students) {
+  if (students instanceof Array) {
+    return students.map((student) => student.id);
+  }
+  return [];
+}
