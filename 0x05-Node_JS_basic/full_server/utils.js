dcef16c2557ce302721d17603a01ac3af570c44b
manonaSamir <bena.yalla@yahoo.com>
2024-08-24 00:31:42 +0300

    push stylecode

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,17 +1,17 @@
-const { readFile } = require("fs");
+const { readFile } = require('fs');
 
-module.exports = function readDatabase(filePath) {
+module.exports = function readDatabase (filePath) {
   const students = {};
   return new Promise((resolve, reject) => {
     readFile(filePath, (err, data) => {
       if (err) {
         reject(err);
       } else {
-        const lines = data.toString().split("\n");
+        const lines = data.toString().split('\n');
         const noHeader = lines.slice(1);
         for (let i = 0; i < noHeader.length; i += 1) {
           if (noHeader[i]) {
-            const field = noHeader[i].toString().split(",");
+            const field = noHeader[i].toString().split(',');
             if (Object.prototype.hasOwnProperty.call(students, field[3])) {
               students[field[3]].push(field[0]);
             } else {
